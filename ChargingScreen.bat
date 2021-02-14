43a631335d913df6be54c5f640e69ce2ac741632
nk125 <nekeselgamer@gmail.com>
2021-02-14 12:15:37 -0600

    Add files via upload

--- ChargingScreen.bat
+++ ChargingScreen.bat
@@ -1,32 +1,52 @@
 @echo off
+
 :subroutine
 :: %1 = Time between points
+
 :: %2 = Loops to charge, example: 1 secs between points + 1 loop = 5 secs charging
-if "%1"="" (
+
+if "%1" EQU "" (
 set "secs=1"
 ) else (
 set "secs=%1"
 )
-if "%2"="" (
+if "%2" EQU "" (
 set "loops=1"
 ) else (
-set "loops=%2"
+set "loops=%2
 )
-:_loop
+:loop
+
 set /a loop+=1
+
 cls
+
 echo Please wait .
+
 timeout /t %secs% /nobreak>nul
+
 cls
+
 echo Please wait  .
+
 timeout /t %secs% /nobreak>nul
+
 cls
+
 echo Please wait   .
+
 timeout /t %secs% /nobreak>nul
+
 cls
+
 echo Please wait    .
+
 timeout /t %secs% /nobreak>nul
+
 cls
+
 echo Please wait     .
+
 timeout /t %secs% /nobreak>nul
-if not %loop%==%loops% goto _loop
+
+if not %loop%==%loops% goto loop

77cc4183c8579fa4bf2c262615f34b5e70b7accb
nk125 <nekeselgamer@gmail.com>
2021-02-14 12:22:20 -0600

    Create ChargingScreen.bat

---
+++ Subroutines/ChargingScreen.bat
@@ -0,0 +1,57 @@
+@echo off
+
+:subroutine
+
+:: %1 = Time between points
+
+:: %2 = Loops to charge, example: 1 sec between points + 1 loop = 5 secs charging
+
+if "%1" EQU "" (
+set "secs=1"
+) else (
+set "secs=%1"
+)
+
+if "%2" EQU "" (
+set "loops=1"
+) else (
+set "loops=%2
+)
+
+:loop
+
+set /a loop+=1
+
+cls
+
+echo Please wait .
+
+timeout /t %secs% /nobreak>nul
+
+cls
+
+echo Please wait  .
+
+timeout /t %secs% /nobreak>nul
+
+cls
+
+echo Please wait   .
+
+timeout /t %secs% /nobreak>nul
+
+cls
+
+echo Please wait    .
+
+timeout /t %secs% /nobreak>nul
+
+cls
+
+echo Please wait     .
+
+timeout /t %secs% /nobreak>nul
+
+if not %loop%==%loops% goto loop
+
+exit /b
